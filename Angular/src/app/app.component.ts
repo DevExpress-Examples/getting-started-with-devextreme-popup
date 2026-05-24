6250ad3bb64eeee5cdf0e0c8ecd1d31fbb483f01
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 11:51:50 +0300

    Angular - Update to use standalone components

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+
 @Component({
     selector: 'app-root',
+    imports: [DxButtonModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
 })
 export class AppComponent {
   isPopupVisible: boolean;
